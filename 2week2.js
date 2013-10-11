6e70d0cd639052500400f5ff5206f53899abfc60
JFingert <joshuafingert@gmail.com>
2013-10-10 19:40:10 -0700

    test

---
+++ 2week2.js
@@ -0,0 +1,40 @@
+for (x = 1; x<11 ; x++){
+  console.log(x)
+}
+
+
+
+
+switch (day){
+  case "Saturday":
+    console.log("Sleepin' in after partying all night!!!");
+    break;
+  case "Sunday":
+    console.log("Sleepin' in after partying all night again!!!");
+    break;
+  case "Monday":
+  case "Tuesday":
+  case "Wednesday":
+  case "Thursday":
+    console.log("Uggh! Work!!!");
+    break;
+  default:
+    console.log("Uggh! Work!!!");
+}
+
+
+
+var day = "Tuesday"
+
+if (day === "Friday"){
+  console.log("TGIF")
+}
+else if(day === "Sunday"){
+  console.log("Sleepin' in, baby!!!")
+}
+else if(day === "Monday"){
+  console.log("Get your tushy to school, fool!!!")
+}
+ else{
+  console.log("I wish it were Friday")
+}
---
+++ e_2.js
@@ -0,0 +1,23 @@
+for (x = .7; x<=10 ; x=x+x){
+  console.log(x)
+}
+
+var myName = "Joshua Fingert";
+
+for (var i = myName.length; i>=0; i--){
+
+console.log(myName[i]);
+
+}
+
+var phrase = "marbles";
+
+for (var x = 1; x<=phrase.length; x++){
+
+	print(phrase[x]);
+
+	if (x>=phrase.length){
+		print(phrase[0] + "ay");
+	}
+}
+//console.log(phrase[x]); console.log(phrase[0]);
